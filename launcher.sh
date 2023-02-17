a7b6cdbbd26be970f231b541cc61483a6f6f4d5e
gregdott <dott.greg@gmail.com>
2023-02-17 16:27:39 +0000

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# launcher.sh
+# Just a template for what is needed to run script on startup on rpi
+# 
+# sudo crontab -e
+# add the following line
+# @reboot sh /home/pi/path-to-this-script/launcher.sh >/home/pi/path-to-log-file 2>&1
+
+sudo python3 /home/pi/cam/motion.py
