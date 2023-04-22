7547a39cddb403739a0ffa4db0983f054eb4cee8
gregdott <dott.greg@gmail.com>
2023-04-22 17:08:04 +0100

    Socket server stuff

---
+++ socket_client.py
@@ -0,0 +1,29 @@
+# Originally taken from https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
+# Minor modifications have been done (if any at all)
+
+import socket
+
+
+def client_program():
+    #host = socket.gethostname()  # as both code is running on same pc
+    host = "206.189.120.165"
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
