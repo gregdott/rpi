63d0c063e64efda79e0fbeeb405d1a6c242e1cc3
gregdott <dott.greg@gmail.com>
2023-04-23 17:31:28 +0100

    rearranging

--- socket/socket_client.py
+++ socket/socket_client.py
@@ -8,23 +8,25 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def client_program():
-    #host = socket.gethostname()  # as both code is running on same pc
-    host = os.getenv('HOST_IP')
+    host = socket.gethostname()  # as both code is running on same pc
+    #host = "192.168.99.1"
+    #host = os.getenv('HOST_IP')
     print(host)
     port = 5000  # socket server port number
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" -> ")  # take input
+    #message = input(" -> ")  # take input
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
+    #while message.lower().strip() != 'bye':
+    while True:
+        #client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
 
-        message = input(" -> ")  # again take input
+        #message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
 
